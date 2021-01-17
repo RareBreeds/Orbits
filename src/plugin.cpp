60a141849985979ed5f6aa1622589f074df4dd63
Rare Breeds <rare.breeds@protonmail.com>
2021-01-17 15:04:21 +0000

    Error handling

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -6,10 +6,9 @@
 Plugin *pluginInstance;
 Config config;
 
-void init(Plugin *p) {
+void init(Plugin *p)
+{
 	pluginInstance = p;
-	config.fromJson(asset::plugin(pluginInstance, "res/eugene-layout.json"));
-	config.loadComponentPositions();
 
 	// Add modules here
 	p->addModel(modelRareBreeds_Orbits_Eugene);
@@ -17,6 +16,8 @@ void init(Plugin *p) {
 
 	// Any other plugin initialization may go here.
 	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
+	config.fromJson(asset::plugin(pluginInstance, "res/eugene-layout.json"));
+	config.loadComponentPositions();
 }
 
 // TODO: Error handling, bad json can make it crash in horrible to debug ways
@@ -41,8 +42,6 @@ void Theme::fromJson(json_t *root)
 }
 
 // TODO: If we can't find the component positions use some sensible defaults
-// TODO: Error handling
-// TODO: Avoid code duplication
 void Config::loadComponentPositions(void)
 {
 	std::ifstream ifs(m_themes[m_default].panel);
@@ -51,23 +50,62 @@ void Config::loadComponentPositions(void)
 
 	// Find all component positions from the svg
 	size_t search = content.find("inkscape:label=\"components\"");
+	if(search == std::string::npos)
+	{
+		return;
+	}
 
 	for(auto i = 0; i < 17; ++i)
 	{
 		search = content.find("<", search);
+		if(search == std::string::npos)
+		{
+			break;
+		}
+
 		search = content.find("x=", search);
+		if(search == std::string::npos)
+		{
+			break;
+		}
+
 		size_t float_start = search + 3;
 		size_t float_end = content.find("\"", float_start);
+		if(float_end == std::string::npos)
+		{
+			break;
+		}
+
 		float x = std::stof(content.substr(float_start, float_end - float_start));
 
 		search = content.find("y=", float_end);
+		if(search == std::string::npos)
+		{
+			break;
+		}
+
 		float_start = search + 3;
 		float_end = content.find("\"", float_start);
+		if(float_end == std::string::npos)
+		{
+			break;
+		}
+
 		float y = std::stof(content.substr(float_start, float_end - float_start));
 
 		search = content.find("inkscape:label=", float_end);
+		if(search == std::string::npos)
+		{
+			break;
+		}
+
 		size_t name_start = search + 16;
 		size_t name_end = content.find("\"", name_start);
+		if(name_end == std::string::npos)
+		{
+			break;
+		}
+
 		std::string name = content.substr(name_start, name_end - name_start);
 
 		Vec pos = mm2px(Vec(x, y));
@@ -149,30 +187,47 @@ void Config::fromJson(std::string path)
 	json_t *root;
 	json_error_t error;
 	root = json_load_file(path.c_str(), 0, &error);
-	if (!root)
+	if(root)
 	{
-		// TODO: Handle errors, the whole of rack crashes if this crashes
-	}
-
-	json_t *themes = json_object_get(root, "themes");
-	size_t index;
-	json_t *value;
-	json_array_foreach(themes, index, value)
-	{
-		Theme theme;
-		theme.fromJson(value);
-		m_themes.push_back(theme);
-	}
-
-	const char *default_name = json_string_value(json_object_get(root, "default"));
-	for(size_t i = 0; i < m_themes.size(); ++i)
-	{
-		if(m_themes[i].name == default_name)
+		json_t *themes = json_object_get(root, "themes");
+		if(themes)
 		{
-			m_default = i;
-			break;
-		}
-	}
+			size_t index;
+			json_t *value;
+			json_array_foreach(themes, index, value)
+			{
+				Theme theme;
+				theme.fromJson(value);
+				m_themes.push_back(theme);
+			}
 
-	json_decref(root);
+			// Default to the first theme if no default theme is provided
+			// Or if it doesn't reference a theme in the themes array
+			m_default = 0;
+			json_t *def = json_object_get(root, "default");
+			if(def)
+			{
+				const char *default_name = json_string_value(def);
+				for(size_t i = 0; i < m_themes.size(); ++i)
+				{
+					if(m_themes[i].name == default_name)
+					{
+						m_default = i;
+						break;
+					}
+				}
+			}
+		}
+		else
+		{
+			// TODO: No themes array
+		}
+
+		json_decref(root);
+	}
+	else
+	{
+		// TODO: Unable to find Eugene config file
+		// Use default assets?
+	}
 }
