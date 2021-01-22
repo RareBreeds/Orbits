a006fbe1ce7d1593751269300908e6d42adde316
Rare Breeds <rare.breeds@protonmail.com>
2021-01-22 20:53:17 +0000

    Remove Polygene from manifest

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -15,7 +15,8 @@ void init(Plugin *p)
 		p->addModel(modelRareBreeds_Orbits_Eugene);
 	}
 
-	p->addModel(modelRareBreeds_Orbits_Polygene);
+	// TODO: Enable Polygene when ready
+	//p->addModel(modelRareBreeds_Orbits_Polygene);
 
 	// Any other plugin initialization may go here.
 	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
